55459726371131eb560cae242ef565a096c07418
Seongwon <surbise@gmail.com>
2023-02-08 14:50:27 +0900

    카카오페이 API연동
    java - Payment.kakao.KakaoPayController 작성, jsp - kakaotest 작성, js - kakaopay.js 작성

--- src/main/java/com/oasis/message/SmsController.java
+++ src/main/java/com/oasis/message/SmsController.java
@@ -4,7 +4,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,9 +23,16 @@ public class SmsController {
 
     private final SmsService smsService;
 
+    // 핸드폰번호 중복체크 후 문자메세지 발송
     @PostMapping("/user/sms")
-    public ResponseEntity<SmsResponse> test(@RequestBody Request request) throws NoSuchAlgorithmException, URISyntaxException, UnsupportedEncodingException, InvalidKeyException, JsonProcessingException {
-        SmsResponse data = smsService.sendSms(request.getRecipientPhoneNumber(), request.getContent());
-        return ResponseEntity.ok().body(data);
+    public ResponseEntity<Map<String, Object>> test(@RequestBody Request request) throws NoSuchAlgorithmException, URISyntaxException, UnsupportedEncodingException, InvalidKeyException, JsonProcessingException {
+    	
+    	String random = smsService.makeRandom();
+    	String msg = "Oasis에서 보낸 인증 번호 : "+random;
+        SmsResponse data = smsService.sendSms(request.getRecipientPhoneNumber(), msg);
+        Map<String, Object> returnMap = new HashMap<String, Object>();
+        returnMap.put("SmsResponse", data);
+        returnMap.put("checkKey", random);
+        return new ResponseEntity<Map<String, Object>>(returnMap,HttpStatus.OK);
     }
 }
---
+++ src/main/webapp/js/kakaopay.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+ $(document).ready(function(){
+ let index = {
+	init:function(){
+		let product = $("#product").val();
+		let orderno = $("#orderno").val();
+		let price = $("#price").val();
+		let userId = $("#userId").val();
+		let count = $("#count").val();
+        $("#btn-kakaopay").on("click", ()=>{ // function(){}를 사용안하고 , ()=>{}를 사용하는 이유는 this를 바인딩하기 위해서
+        let payment = {
+        product:$("#product").val(),
+        orderno:$("#orderno").val(),
+        price:$("#price").val(),
+        userId:$("#userId").val(),
+        count:$("#count").val()
+        };
+        console.log(payment);
+			this.kakaopay(payment);
+		});
+	},
+
+  // 카카오페이 결제
+	kakaopay:function(payment){
+		
+		$.ajax({
+			type:'post',
+			url:"/Oasis/kakaopay",
+			data:JSON.stringify(payment),
+			dataType:"json",
+			headers: { 'Content-Type': 'application/json' }
+		}).done(function(resp){
+			if(resp.status === 500){
+				alert("카카오페이결제를 실패하였습니다.")
+			} else{
+				 // alert(resp.tid); //결제 고유 번호
+				var box = resp.next_redirect_pc_url;
+				//window.open(box); // 새창 열기
+				location.href = box;
+			}
+		
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
+		
+	},
+}
+
+index.init();
+});
