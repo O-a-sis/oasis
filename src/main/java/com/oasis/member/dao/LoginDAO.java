616ed2a4935ca8e648f22f3d3428cf0ed6be87b3
maengjivv <glaaudwls@naver.com>
2023-02-09 10:05:17 +0900

    흐름도 - 회원관리 - 회원가입, 메뉴탭

---
+++ src/main/java/com/oasis/member/dao/LoginDAO.java
@@ -0,0 +1,10 @@
+package com.oasis.member.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.oasis.common.dao.AbstractDAO;
+
+@Repository("loginDAO")
+public class LoginDAO extends AbstractDAO{
+
+}
