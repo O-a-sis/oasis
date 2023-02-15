3f01861850e5e42a75df42dd52e939fc4bda858a
Loopy <s__0118@naver.com>
2023-02-15 14:23:51 +0900

    본사 - 지점관리 (지점리스트 / 지점상세보기)
    java  > main > 3개 폴더 다 변경 - 페이징 및 회원정보 수정 변경
    Admin_SQL.xml 변경 - 검색 및 페이징 추가
    storeUpdate.js 변경
    admin - jsp - storeList / storeDetail

--- src/main/java/com/oasis/admin/controller/AdminStoreController.java
+++ src/main/java/com/oasis/admin/controller/AdminStoreController.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.oasis.admin.service.AdminStoreService;
 import com.oasis.common.CommandMap;
+import com.oasis.common.util.Paging;
 
 import lombok.AllArgsConstructor;
 
@@ -55,25 +58,63 @@ public class AdminStoreController {
 
 		return mv;
 	}
+	
+	/* 지점명 중복체크 */
+	   @RequestMapping(value="/confirmName.oa", method=RequestMethod.POST)
+	   @ResponseBody
+	   public boolean checkName(@RequestParam String name)throws Exception{
+	      
+	      int check=adminStoreService.getNameCheck(name);
+	      
+	      if(check>0) {
+	         return true;
+	      }else 
+	         return false;
+	   }
+	   
+	   
+	   
+	   
 
 	// 관리자 - 지점 상세보기
 	@RequestMapping(value = "/storeDetail.oa")
 	public ModelAndView storeDetail(CommandMap commandMap) throws Exception {
 		ModelAndView mv = new ModelAndView("admin/storeDetail");
 		
+		/* 페이징을 위한 변수 */
+		int page = commandMap.get("page") == null ? 1 : Integer.parseInt((String) commandMap.get("page"));// 현재 페이지
+		int pageSize = 7;// 한 페이지에 보여줄 행의 수
+		int start = (page * pageSize) - pageSize + 1;
+		int end = page * pageSize;
+		int ListCount = 0; // 전체 게시글 수
+		int pageBlock = 5; // 표시할 페이지의 수
+		String url = "storeDetail.oa";
+		String searchUrl = "";
+		commandMap.put("START", start);
+		commandMap.put("END", end);		
+
 		List<Map<String, Object>> listSum = adminStoreService.storeSumRank(commandMap.getMap());
 		mv.addObject("listSum", listSum);
+	
+
+		if (listSum.size() > 0) {
+			ListCount = Integer.parseInt(String.valueOf(listSum.get(0).get("TOTAL_COUNT")));
+		}
+		// 페이징할 아이템의 총 수, 페이지의 수 ex> 1~5 6~10, 한 페이지에 표시할 게시글의 수, 현재 페이지, 이동주소, 검색시 사용할
+		// 주소 입력
+		Paging paging = new Paging(ListCount, pageBlock, pageSize, page, url, searchUrl);
 
 		Map<String, Object> map = adminStoreService.storeDetail(commandMap.getMap());
 		mv.addObject("map", map);
-		System.out.println(map);
-
+		
+		
+		mv.addObject("paging", paging);
 		return mv;
 	}
+	
 
 	// 관리자 - 지점수정
-	@RequestMapping(value = "/update", method = { RequestMethod.PATCH,
-			RequestMethod.PUT }, consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
+	@RequestMapping(value = "/update", method = RequestMethod.POST, consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
 	public ResponseEntity<String> update(@RequestBody Map<String, Object> map) throws Exception {
 		int count = adminStoreService.storeUpdate(map);
 		return count == 1 ? new ResponseEntity<String>("success", HttpStatus.OK)
